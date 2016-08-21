ef2a9bc6d13a0dd17baf8989c4ef7cd2ff7f473b
Robert <custard130@hotmail.com>
2016-08-21 12:30:34 +0100

    fixed controller path in routes

--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,9 @@
 <?php 
 
 Route::group(['prefix' => 'auth/social'], function(){
-	Route::post('social/details', 'RMoore\SocialiteExtension\Controllers/SocialController@setUserDetails');
-	Route::get('social/details', 'RMoore\SocialiteExtension\Controllers/SocialController@getUserDetailsForm');
-	Route::get('social/connect', ['uses' => 'RMoore\SocialiteExtension\Controllers/SocialController@getConnectPage', 'as' => 'auth.social.connect', 'middleware' => ['auth']]);
-	Route::get('social/{provider}', ['uses'=>'RMoore\SocialiteExtension\Controllers/SocialController@redirectToProvider', 'as' => 'auth.social.redirect']);
-	Route::get('social/{provider}/callback', ['uses'=>'RMoore\SocialiteExtension\Controllers/SocialController@handleProviderCallback', 'as'=>'auth.social.callback']);
+	Route::post('social/details', 'RMoore\SocialiteExtension\Controllers\SocialController@setUserDetails');
+	Route::get('social/details', 'RMoore\SocialiteExtension\Controllers\SocialController@getUserDetailsForm');
+	Route::get('social/connect', ['uses' => 'RMoore\SocialiteExtension\Controllers\SocialController@getConnectPage', 'as' => 'auth.social.connect', 'middleware' => ['auth']]);
+	Route::get('social/{provider}', ['uses'=>'RMoore\SocialiteExtension\Controllers\SocialController@redirectToProvider', 'as' => 'auth.social.redirect']);
+	Route::get('social/{provider}/callback', ['uses'=>'RMoore\SocialiteExtension\Controllers\SocialController@handleProviderCallback', 'as'=>'auth.social.callback']);
 });
